52da5c5db64e898ccd741a1a6c2559194ec99f13
Sayantan Pal <sayantanpal2000.2018@gmail.com>
2023-06-25 23:43:58 +0530

    not working

--- Player.gd
+++ Player.gd
@@ -11,14 +11,17 @@ var health:int = 100
 
 @rpc("any_peer")
 func damage(amount):
-	if not is_multiplayer_authority(): return
+#	if not is_multiplayer_authority(): return
 	if health > amount:
 		health -= amount
 	else:
 		health = 0
 	print("health: " + str(health))
 	print("id: " + str(multiplayer.get_unique_id()))
-		
+#	var newMaterial = CanvasItemMaterial.new() #Make a new Spatial Material
+#	newMaterial.albedo_color = Color(0.92, 0.69, 0.13, 1.0) #Set color of new material
+#	$MeshInstance2D.material_override = newMaterial #Assign new material to material overrride
+
 
 func _enter_tree():
 	set_multiplayer_authority(str(name).to_int())
@@ -28,6 +31,7 @@ func _ready():
 	$Camera2D.make_current()
 
 func _physics_process(_delta):
+	$MeshInstance2D.modulate = Color(health, 0, 0)
 	if not is_multiplayer_authority(): return
 	
 	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down").normalized()
@@ -37,6 +41,7 @@ func _physics_process(_delta):
 	
 	if Input.is_key_pressed(KEY_SPACE):
 		use_ability(ability)
+		
 
 func use_ability(ability):
 	if not is_multiplayer_authority(): return
